d72af3115a2a24da8dd2fb0183b424d22ffb0e7e
Mike Rousos <Mike@MJRousos.com>
2015-12-22 23:36:54 -0500

    Update gulp file and pre/post run scripts to use ./assets
    Pull from ./assets and deploy to ./wwwroot.

--- RiskSimFrontEnd/gulpfile.js
+++ RiskSimFrontEnd/gulpfile.js
@@ -9,15 +9,21 @@ var gulp = require("gulp"),
   project = require("./project.json");
 
 var paths = {
+  assets: "./assets/",
   webroot: "./wwwroot/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.js = paths.assets + "scripts/**/*.js";
+paths.minJs = paths.assets + "scripts/**/*.min.js";
+paths.css = paths.assets + "styles/**/*.css";
+paths.minCss = paths.assets + "styles/**/*.min.css";
+paths.images = paths.assets + "images/**/*.*";
+
+paths.concatJsDest = paths.webroot + "scripts/site.min.js";
+paths.concatCssDest = paths.webroot + "styles/site.min.css";
+paths.copyJsDest = paths.webroot + "scripts/site.js";
+paths.copyCssDest = paths.webroot + "styles/site.css";
+paths.imagesDest = paths.webroot + "images";
 
 gulp.task("clean:js", function(cb) {
   rimraf(paths.concatJsDest, cb);
@@ -39,10 +45,31 @@ gulp.task("min:js", function() {
 });
 
 gulp.task("min:css", function() {
-  gulp.src([paths.css, "!" + paths.minCss])
-    .pipe(concat(paths.concatCssDest))
-    .pipe(cssmin())
+     gulp.src([paths.css, "!" + paths.minCss])
+     .pipe(concat(paths.concatCssDest))
+     .pipe(cssmin())
+     .pipe(gulp.dest("."));
+});
+
+gulp.task("copy:js", function() {
+  gulp.src([paths.js, "!" + paths.minJs], {
+      base: "."
+    })
+    .pipe(concat(paths.copyJsDest))
     .pipe(gulp.dest("."));
 });
 
+gulp.task("copy:css", function() {
+  gulp.src([paths.css, "!" + paths.minCss])
+    .pipe(concat(paths.copyCssDest))
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("copy:images", function() {
+  gulp.src([paths.images])
+    .pipe(gulp.dest(paths.imagesDest));
+});
+
 gulp.task("min", ["min:js", "min:css"]);
+gulp.task("copy", ["copy:js", "copy:css", "copy:images"]);
+gulp.task("deploy", ["min","copy"]);
