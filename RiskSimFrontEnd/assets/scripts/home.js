6cb5a3680c161e226c8fe65c9681d2ca309db4a0
Mike Rousos <mikerou@microsoft.com>
2015-12-29 21:41:17 -0800

    Finish implementing view (modulo the chart) and begin on js

---
+++ RiskSimFrontEnd/assets/scripts/home.js
@@ -0,0 +1,27 @@
+﻿function initializeHomeIndex()
+{
+    // Attach toggles
+    $("#attackingOptionsExpand").click(function () {
+        $("#optionalAttackingInputs").slideToggle();
+        $("#attackingOptionsExpand").toggleClass("glyphicon-plus glyphicon-minus");
+    });
+
+    $("#defendingOptionsExpand").click(function () {
+        $("#optionalDefendingInputs").slideToggle();
+        $("#defendingOptionsExpand").toggleClass("glyphicon-plus glyphicon-minus");
+    });
+
+    // Attach simulate button
+    $("#simulateButton").click(simulate);
+}
+
+// Make call to get simulated result
+function simulate()
+{
+    // Disable the simulate button until this sim is done
+    $("#simulateButton").attr("disabled", true).html("Simulating...");
+
+    // TODO
+
+    $("#results").show();
+}
