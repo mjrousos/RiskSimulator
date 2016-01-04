ab6505c36e8497f17127350cc39e5743428db89c
Mike Rousos <Mike@MJRousos.com>
2016-01-04 15:28:45 -0500

    Display summary and chart data via jquery after simulation

--- RiskSimFrontEnd/assets/scripts/home.js
+++ RiskSimFrontEnd/assets/scripts/home.js
@@ -13,6 +13,9 @@
 
     // Attach simulate button
     $("#simulateButton").click(simulate);
+
+    // Note that charts should be responsive
+    Chart.defaults.global.responsive = true;
 }
 
 // Make call to get simulated result
@@ -21,7 +24,47 @@ function simulate()
     // Disable the simulate button until this sim is done
     $("#simulateButton").attr("disabled", true).html("Simulating...");
 
-    // TODO
+    // TODO : Get chart data via an API
+    var data = {};
+    data.SuccessChance = 0.63131784;
+    data.ChartData = {
+        labels: ["-1", "+1", "+2", "+3", "+4"],
+        datasets: [
+            {
+                fillColor: "rgba(120, 210, 120, 0.7)",
+                strokeColor: "rgba(120, 210, 120, 0.9)",
+                highlightFill: "rgba(120, 210, 120, 0.9)",
+                highlightStroke: "rgba(120, 210, 120, 1)",
+                data: [0.19, 0.21, 0.24, 0.18, 0.1]
+            }
+        ]
+    };
+    data.options = {
+        animation: true,
+        scaleOverride: true,
+        scaleStartValue: 0,
+        scaleStepWidth: 0.1,
+        scaleSteps: 5
+    };
 
+    // Re-enable the simulate button when the sim is done
+    $("#simulateButton").attr("disabled", false).html("Simulate");
     $("#results").show();
+
+    // Get context with jQuery - using jQuery's .get() method.
+    var ctx = $("#outcomeChart").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var outcomeChart = new Chart(ctx).Bar(data.ChartData, data.options);
+    for (var i = 0; i < data.ChartData.labels.length; i++) {
+        if (data.ChartData.labels[i] < 0)
+        {
+            outcomeChart.datasets[0].bars[i].fillColor = "rgba(210, 120, 120, 0.7)";
+            outcomeChart.datasets[0].bars[i].strokeColor = "rgba(210, 120, 120, 0.9)";
+            outcomeChart.datasets[0].bars[i].highlightFill = "rgba(210, 120, 120, 0.9)";
+            outcomeChart.datasets[0].bars[i].highlightStroke = "rgba(210, 120, 120, 1)";
+        }
+    };
+
+    // Add summary
+    $("#resultsSummary").html((data.SuccessChance * 100).toFixed(2) + "% chance of victory");
 }
